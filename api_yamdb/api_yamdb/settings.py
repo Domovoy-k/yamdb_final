89f64b435c7ea09e06199188c847dc9d02531fec
Domovoy <afss_rf@list.ru>
2022-10-16 22:41:30 +1000

    work in progress_48

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -104,7 +104,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [BASE_DIR + '/reviews/static']
+STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
